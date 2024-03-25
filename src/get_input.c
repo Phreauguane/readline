6fa93adf3e3e59ff872fa8201390e1f4947c21ae
Larz <larz@Larz.myguest.virtualbox.org>
2024-03-25 12:29:04 +0100

    miau

--- src/get_input.c
+++ src/get_input.c
@@ -6,29 +6,57 @@
 /*   By: jde-meo <jde-meo@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/19 00:32:14 by jde-meo           #+#    #+#             */
-/*   Updated: 2024/03/19 00:39:51 by jde-meo          ###   ########.fr       */
+/*   Updated: 2024/03/19 13:31:14 by jde-meo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ft_readline.h>
 
-void	read_key(int fd, char *buff, int size)
+void	read_esc(char **buff)
 {
-	
+	char	c;
+	int		ret;
+
+	c = '\0';
+	ret = read(STDIN_FILENO, &c, 1);
+	while (ret == 0)
+		ret = read(STDIN_FILENO, &c, 1);
+	if (c != '\133')
+		return ;
+	ret = read(STDIN_FILENO, &c, 1);
+	while (ret == 0)
+		ret = read(STDIN_FILENO, &c, 1);
+	if (c != '\133')
+		return ;
+	(void)ret;
+	(void)buff;
+}
+
+void	read_key(char **buff)
+{
+	char	c;
+	int		ret;
+
+	ret = read(STDIN_FILENO, &c, 1);
+	while (c != '\n' && ret > 0)
+	{
+		if (c == '\033')
+			read_esc(buff);
+		else
+		{
+			*buff = str_addc(*buff, c);
+			ft_printf("%c", c);
+		}
+		ret = read(STDIN_FILENO, &c, 1);
+	}
 }
 
 void	read_input(t_rldata *data)
 {
 	char	*buff;
-	char	c;
 
 	buff = NULL;
 	(void)data;
-	c = '\0';
-	while (c != '\n' && read(STDIN_FILENO, &c, 1))
-	{
-		buff = str_addc(buff, c);
-		ft_printf("%c", c);
-	}
-	ft_printf(buff);
+	read_key(&buff);
+	ft_printf("\'%s\'", buff);
 }
